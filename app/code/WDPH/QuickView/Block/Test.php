3921323b5386f6a6a44ba1636e687dbff78d5966
Marcus Kincaid <marcus@queldorei.com>
2017-12-14 16:59:03 +0200

    Controller declared, test block added

---
+++ app/code/WDPH/QuickView/Block/Test.php
@@ -0,0 +1,11 @@
+<?php
+namespace WDPH\QuickView\Block;
+ 
+class Test extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorld()
+    {
+        return 'Hello world!';
+    }
+}
+?>
