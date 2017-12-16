9f97fcfd2d9e328f53668ad0aa9b4f3f82c9306e
Marcus Kincaid <marcus@queldorei.com>
2017-12-16 18:08:20 +0200

    Test block/template removed. Now controller extend \Magento\Catalog\Controller\Product\View. Plugin which add quick view url added

--- app/code/WDPH/QuickView/Controller/Catalog/Product/View.php
+++ app/code/WDPH/QuickView/Controller/Catalog/Product/View.php
@@ -3,20 +3,7 @@
 namespace WDPH\QuickView\Controller\Catalog\Product;
 use Magento\Framework\App\Action\Context;
  
-class View extends \Magento\Framework\App\Action\Action
-{
-    protected $_resultPageFactory;
- 
-    public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory)
-    {
-        $this->_resultPageFactory = $resultPageFactory;
-        parent::__construct($context);
-    }
- 
-    public function execute()
-    {
-        $resultPage = $this->_resultPageFactory->create();
-        return $resultPage;
-    }
+class View extends \Magento\Catalog\Controller\Product\View
+{    
 }
 ?>
