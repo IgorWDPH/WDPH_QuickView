c7d06149e7cb77be82e9fb0c67906ce7cb484b0c
Marcus Kincaid <marcus@queldorei.com>
2017-12-21 14:30:05 +0200

    css class renamed

--- app/code/WDPH/QuickView/Plugin/BlockProductList.php
+++ app/code/WDPH/QuickView/Plugin/BlockProductList.php
@@ -9,13 +9,13 @@ class BlockProductList
     protected $scopeConfig;
 	
     public function __construct(\Magento\Framework\UrlInterface $urlInterface, \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
-	{
+	{		
         $this->urlInterface = $urlInterface;
         $this->scopeConfig = $scopeConfig;
     }
 
     public function aroundGetProductDetailsHtml(\Magento\Catalog\Block\Product\ListProduct $subject, \Closure $proceed, \Magento\Catalog\Model\Product $product)
-    {
+    {		
         $result = $proceed($product);
         //$isEnabled = $this->scopeConfig->getValue(self::XML_PATH_QUICKVIEW_ENABLED,  \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         if(true)
@@ -23,7 +23,7 @@ class BlockProductList
             //$buttonStyle =  'wdph_quickview_button_' . $this->scopeConfig->getValue(self::XML_PATH_QUICKVIEW_BUTTONSTYLE,  \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 			$buttonStyle =  'wdph_quickview_button';
             $productUrl = $this->urlInterface->getUrl('wdph_quickview/catalog_product/view', array('id' => $product->getId()));
-            return $result . '<a class="weltpixel-quickview '.$buttonStyle.'" data-quickview-url=' . $productUrl . ' href="javascript:void(0);"><span>' . __("Quickview") . '</span></a>';
+            return $result . '<a class="wdph-quickview '.$buttonStyle.'" data-quickview-url=' . $productUrl . ' href="javascript:void(0);"><span>' . __("Quickview") . '</span></a>';
         }        
         return $result;
     }
