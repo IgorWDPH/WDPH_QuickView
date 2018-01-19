ce254715bc45f1e7ce4223724049b9636da827b8
Marcus Kincaid <marcus@queldorei.com>
2018-01-19 21:10:25 +0200

    Observer code cleaned. PrevNext removed from layout

--- app/code/WDPH/QuickView/Model/Observer/AddUpdateHandlesObserver.php
+++ app/code/WDPH/QuickView/Model/Observer/AddUpdateHandlesObserver.php
@@ -2,49 +2,41 @@
 namespace WDPH\QuickView\Model\Observer;
 
 class AddUpdateHandlesObserver implements \Magento\Framework\Event\ObserverInterface
-{   
-    protected $scopeConfig;
-    protected $request;
-    protected $_storeManager;
-    protected $productRepository;
+{    
 	protected $quickviewHelper;
     
-    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-                                \Magento\Framework\App\Request\Http $request,
-                                \Magento\Store\Model\StoreManagerInterface $storeManager,
-                                \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-								\WDPH\QuickView\Helper\Data $quickviewHelper)
+    public function __construct(\WDPH\QuickView\Helper\Data $quickviewHelper)
     {
-		$this->quickviewHelper = $quickviewHelper;
-        $this->scopeConfig = $scopeConfig;
-        $this->request = $request;
-        $this->_storeManager = $storeManager;
-        $this->productRepository = $productRepository;
+		$this->quickviewHelper = $quickviewHelper;        
     }    
    
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $layout = $observer->getData('layout');
-        if($observer->getData('full_action_name') != 'wdph_quickview_catalog_product_view') 
+		if($this->quickviewHelper->getConfig('general/enabled'))
 		{
-            return $this;
-        }        
-        if($this->quickviewHelper->getConfig('general/hide_product_info_details'))
-		{
-            $layout->getUpdate()->addHandle('wdph_quickview_removeproductinfo_details');
-        }
-		if($this->quickviewHelper->getConfig('general/hide_related'))
-		{				
-            $layout->getUpdate()->addHandle('wdph_quickview_hide_related');
-        }
-		if($this->quickviewHelper->getConfig('general/hide_upsell'))
-		{				
-            $layout->getUpdate()->addHandle('wdph_quickview_hide_upsell');
-        }
-		if($this->quickviewHelper->getConfig('general/hide_social'))
-		{				
-            $layout->getUpdate()->addHandle('wdph_quickview_hide_social');
-        }
-        return $this;
+			$layout = $observer->getData('layout');
+			if($observer->getData('full_action_name') != 'wdph_quickview_catalog_product_view') 
+			{
+				return $this;
+			}        
+			if($this->quickviewHelper->getConfig('general/hide_product_info_details'))
+			{
+				$layout->getUpdate()->addHandle('wdph_quickview_removeproductinfo_details');
+			}
+			if($this->quickviewHelper->getConfig('general/hide_related'))
+			{				
+				$layout->getUpdate()->addHandle('wdph_quickview_hide_related');
+			}
+			if($this->quickviewHelper->getConfig('general/hide_upsell'))
+			{				
+				$layout->getUpdate()->addHandle('wdph_quickview_hide_upsell');
+			}
+			if($this->quickviewHelper->getConfig('general/hide_social'))
+			{				
+				$layout->getUpdate()->addHandle('wdph_quickview_hide_social');
+			}
+			return $this;
+		}        
     }
 }
+?>
